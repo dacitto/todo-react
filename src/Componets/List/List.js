9a9f3111261dbfad84286226ecae212e4dc1422f
dacitto <dacidigital@gmail.com>
2021-10-24 23:47:41 +0100

    feat: add Task and List components

--- src/Componets/List/List.js
+++ src/Componets/List/List.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./List.scss";
+import Task from "../Task/Task";
 const List = () => {
-  return <div className="list container"></div>;
+  return (
+    <div className="list container">
+      <Task checked={"active"}></Task>
+      <Task></Task>
+      <Task checked={"active"}></Task>
+    </div>
+  );
 };
 
 export default List;
